c35e1bb2616d93470bd7388c4a0f5fb50347a050
Romil-Sharma <romil3711@gmail.com>
2026-04-22 00:07:55 +0530

    feat: complete initial MyInfo java excercise

---
+++ 01_Basics/src/MyInfo.java
@@ -0,0 +1,11 @@
+class MyInfo
+{
+    public static void main(String args[])
+    {
+        int yearOfBirth = 2000;
+        String fav_Prog_Lang = "Javascript";
+        boolean isLearningJava = true;
+
+        System.out.println("Date of Birth : "+yearOfBirth+", Favourite Programming Language: "+ fav_Prog_Lang +", Are you lrarning Java: " + isLearningJava+".");
+    }
+}
