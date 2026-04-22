543684c9787e2baab864e7fd397a85b7bf93200b
Romil-Sharma <romil3711@gmail.com>
2026-04-22 13:59:55 +0530

    feat: iterate over list of encapsulated students

--- 01_Basics/src/encapsulation/Main.java
+++ 01_Basics/src/encapsulation/Main.java
@@ -1,4 +1,5 @@
 package encapsulation;
+import java.util.ArrayList;
 
 import encapsulation.Student;
 
@@ -11,10 +12,32 @@ public class Main
         s.setName("Bruce");
         s.setRollNumber(103);
 
-        s.setName("John");
-        s.setRollNumber(104);
+        System.out.println(s.getName());
+        System.out.println(s.getRollNumber());
+
+        s.setName("John"); //Bruce got overwritten by "John"
+        s.setRollNumber(104); //103 got overwritten by 104
 
         System.out.println(s.getName());
         System.out.println(s.getRollNumber());
+
+        //To print all students and rollNumber we need to store them in an ArrayList
+        ArrayList<Student> list = new ArrayList<>();
+
+        Student s1 = new Student();
+        s1.setName(s.getName()); //Same as s1.setName("John")
+        s1.setRollNumber(s.getRollNumber()); //Same as s1.setRollNumber(104)
+        list.add(s1);
+
+        Student s2 = new Student();
+        s2.setName("Bruce");
+        s2.setRollNumber(103);
+        list.add(s2);
+
+        for(Student i: list)
+        {
+            System.out.println(i.getName()+","+i.getRollNumber());
+        }
+
     }
 }
