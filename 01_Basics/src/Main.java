f348ad3185590b52b0264edd347a3be77be85e75
Romil-Sharma <romil3711@gmail.com>
2026-04-22 10:33:42 +0530

    feat: add ArrayList of Student objects and iterate using for-each loop

--- 01_Basics/src/Main.java
+++ 01_Basics/src/Main.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
+
 class Main
 {
     public static void main(String args[])
     {
         Student s1 = new Student("Shubham", 101);
         s1.displayInfo();
+
+        //1.) Create an ArrayList of Student objects
+        ArrayList<Student> s2 = new ArrayList<>();
+
+        //2.) Add at least 2 different students to that list.
+        s2.add(new Student("Ajay", 102));
+        s2.add(new Student("Brijesh", 103));
+
+        //3.) Use a for-each loop to print the info for all students in the list
+        for (Student i:s2)
+        {
+            i.displayInfo();
+        }
     }
 }
