d5f878cf1f4e3ce926c7b2ebf90b94e0f9b88380
Romil-Sharma <romil3711@gmail.com>
2026-04-22 08:10:18 +0530

    feat: Control flow - if-else and for-loop

---
+++ 01_Basics/src/ControlFlow.java
@@ -0,0 +1,20 @@
+class CheckAge
+{
+    public static void main(String args[])
+    {
+        int userAge = 19;
+        if(userAge >= 18)
+        {
+            System.out.println("Adult");
+        }
+        else
+        {
+            System.out.println("Minor");
+        }
+
+        for(int i=1; i<=5; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
